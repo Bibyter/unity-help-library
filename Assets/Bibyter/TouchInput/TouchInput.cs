163fac7e5c704e21bed39e9425f8b12e066aef5f
Bibyter <nikitatkarz@gmail.com>
2021-11-01 17:57:23 +0700

    init

---
+++ Assets/Bibyter/TouchInput/TouchInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Bibyter
+{
+    [DefaultExecutionOrder(-1000)]
+    public sealed class TouchInput : MonoBehaviour
+    {
+        public Vector2 percentSwipeDelta { private set; get; }
+
+        Vector2 _prevMousePosition;
+
+        private void OnEnable()
+        {
+            _prevMousePosition = Input.mousePosition;
+        }
+
+        private void Update()
+        {
+            var mousePosition = (Vector2)Input.mousePosition;
+            var canSetSwipe = Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0);
+
+            percentSwipeDelta = canSetSwipe ? (mousePosition - _prevMousePosition) / GetScreenMinSide() : Vector2.zero;
+
+            _prevMousePosition = mousePosition;
+        }
+
+        private int GetScreenMinSide()
+        {
+            return Mathf.Min(Screen.width, Screen.height);
+        }
+    }
+}
