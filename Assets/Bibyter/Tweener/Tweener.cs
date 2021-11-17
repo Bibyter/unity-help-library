3b73cc7147595ac5ca7ffe9a65b38a645415abb6
Bibyter <nikitatkarz@gmail.com>
2021-11-17 11:17:33 +0700

    add tweener

---
+++ Assets/Bibyter/Tweener/Tweener.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bibyter
+{
+    public static class Tweener
+    {
+        public static IEnumerator JumpToTransformTween(Transform transform, Transform target, Vector3 targetOffset, float duration, System.Action onComplete)
+        {
+            float time = 0f;
+            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+            var startPosition = transform.position;
+
+            while (time <= 1f)
+            {
+                time += Time.deltaTime / duration;
+
+                transform.position = Vector3.Lerp(startPosition, target.TransformPoint(targetOffset), curve.Evaluate(time)) + new Vector3(0f, Mathf.Sin(time * Mathf.PI), 0f);
+
+                yield return null;
+            }
+
+            onComplete?.Invoke();
+        }
+
+        public static IEnumerator MoveToTransformTween(Transform transform, Transform target, Vector3 targetOffset, float duration, System.Action onComplete)
+        {
+            float time = 0f;
+            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+            var startPosition = transform.position;
+
+            while (time <= 1f)
+            {
+                time += Time.deltaTime / duration;
+
+                transform.position = Vector3.Lerp(startPosition, target.TransformPoint(targetOffset), curve.Evaluate(time));
+
+                yield return null;
+            }
+
+            onComplete?.Invoke();
+        }
+    }
+}
