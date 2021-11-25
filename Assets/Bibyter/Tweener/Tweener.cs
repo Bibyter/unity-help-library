48694f377348789b1ea17c62f9cf0ec84b17626e
Bibyter <nikitatkarz@gmail.com>
2021-11-25 12:40:00 +0700

    update tweener

--- Assets/Bibyter/Tweener/Tweener.cs
+++ Assets/Bibyter/Tweener/Tweener.cs
@@ -6,6 +6,42 @@ namespace Bibyter
 {
     public static class Tweener
     {
+        public static IEnumerator RotateTo(Transform transform, Quaternion targetRotation, float duration, System.Action onComplete)
+        {
+            float time = 0f;
+            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+            var startRotation = transform.rotation;
+
+            while (time <= 1f)
+            {
+                time += Time.deltaTime / duration;
+
+                transform.rotation = Quaternion.Lerp(startRotation, targetRotation, curve.Evaluate(time));
+
+                yield return null;
+            }
+
+            onComplete?.Invoke();
+        }
+
+        public static IEnumerator JumpToPointTween(Transform transform, Vector3 target, float height, float duration, System.Action onComplete)
+        {
+            float time = 0f;
+            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+            var startPosition = transform.position;
+
+            while (time <= 1f)
+            {
+                time += Time.deltaTime / duration;
+
+                transform.position = Vector3.Lerp(startPosition, target, curve.Evaluate(time)) + new Vector3(0f, Mathf.Sin(time * Mathf.PI) * height, 0f);
+
+                yield return null;
+            }
+
+            onComplete?.Invoke();
+        }
+
         public static IEnumerator JumpToTransformTween(Transform transform, Transform target, Vector3 targetOffset, float height, float duration, System.Action onComplete)
         {
             float time = 0f;
