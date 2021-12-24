3936fa7aec4ee1358669483a6cad79d2be562974
Bibyter <nikitatkarz@gmail.com>
2021-12-24 10:42:53 +0700

    update tweener

--- Assets/Bibyter/Tweener/Tweener.cs
+++ Assets/Bibyter/Tweener/Tweener.cs
@@ -6,18 +6,30 @@ namespace Bibyter
 {
     public static class Tweener
     {
+        public static IEnumerator ScaleTo(Transform transform, Vector3 scale, float duration, System.Action onComplete)
+        {
+            float time = 0f;
+            var from = transform.localScale;
+
+            while (time <= 1f)
+            {
+                time += Time.deltaTime / duration;
+                transform.localScale = Vector3.Lerp(from, scale, InOutQuad(time));
+                yield return null;
+            }
+
+            onComplete?.Invoke();
+        }
+
         public static IEnumerator RotateTo(Transform transform, Quaternion targetRotation, float duration, System.Action onComplete)
         {
             float time = 0f;
-            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
             var startRotation = transform.rotation;
 
             while (time <= 1f)
             {
                 time += Time.deltaTime / duration;
-
-                transform.rotation = Quaternion.Lerp(startRotation, targetRotation, curve.Evaluate(time));
-
+                transform.rotation = Quaternion.Lerp(startRotation, targetRotation, InOutQuad(time));
                 yield return null;
             }
 
@@ -27,15 +39,12 @@ namespace Bibyter
         public static IEnumerator JumpToPointTween(Transform transform, Vector3 target, float height, float duration, System.Action onComplete)
         {
             float time = 0f;
-            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
             var startPosition = transform.position;
 
             while (time <= 1f)
             {
                 time += Time.deltaTime / duration;
-
-                transform.position = Vector3.Lerp(startPosition, target, curve.Evaluate(time)) + new Vector3(0f, Mathf.Sin(time * Mathf.PI) * height, 0f);
-
+                transform.position = Vector3.Lerp(startPosition, target, InOutQuad(time)) + new Vector3(0f, Mathf.Sin(time * Mathf.PI) * height, 0f);
                 yield return null;
             }
 
@@ -45,15 +54,12 @@ namespace Bibyter
         public static IEnumerator JumpToTransformTween(Transform transform, Transform target, Vector3 targetOffset, float height, float duration, System.Action onComplete)
         {
             float time = 0f;
-            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
             var startPosition = transform.position;
 
             while (time <= 1f)
             {
                 time += Time.deltaTime / duration;
-
-                transform.position = Vector3.Lerp(startPosition, target.TransformPoint(targetOffset), curve.Evaluate(time)) + new Vector3(0f, Mathf.Sin(time * Mathf.PI) * height, 0f);
-
+                transform.position = Vector3.Lerp(startPosition, target.TransformPoint(targetOffset), InOutQuad(time)) + new Vector3(0f, Mathf.Sin(time * Mathf.PI) * height, 0f);
                 yield return null;
             }
 
@@ -63,19 +69,61 @@ namespace Bibyter
         public static IEnumerator MoveToTransformTween(Transform transform, Transform target, Vector3 targetOffset, float duration, System.Action onComplete)
         {
             float time = 0f;
-            var curve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
             var startPosition = transform.position;
 
             while (time <= 1f)
             {
                 time += Time.deltaTime / duration;
+                transform.position = Vector3.Lerp(startPosition, target.TransformPoint(targetOffset), InOutQuad(time));
+                yield return null;
+            }
 
-                transform.position = Vector3.Lerp(startPosition, target.TransformPoint(targetOffset), curve.Evaluate(time));
+            onComplete?.Invoke();
+        }
+
+        public static IEnumerator MoveToPoint(Transform transform, Vector3 point, float duration, System.Action onComplete)
+        {
+            float time = 0f;
+            var startPosition = transform.position;
+
+            while(time <= 1f)
+            {
+                time += Time.deltaTime / duration;
+                transform.position = Vector3.Lerp(startPosition, point, InOutQuad(time));
+                yield return null;
+            }
+
+            onComplete?.Invoke();
+        }
+
+        public static IEnumerator MoveWithPath(Transform transform, List<Vector3> path, float speed, System.Action onComplete)
+        {
+            var position = transform.position;
+            int pointId = 0;
+
+            while(pointId < path.Count)
+            {
+                position = Vector3.MoveTowards(position, path[pointId], Time.deltaTime * speed);
+                transform.position = position;
+
+
+                if (Vector3.Distance(position, path[pointId]) < 0.05f)
+                {
+                    pointId++;
+                }
 
                 yield return null;
             }
 
             onComplete?.Invoke();
+        }
+
+        public static float InOutQuad(float value)
+        {
+            value *= 2f;
+            if (value < 1f) return 1f / 2f * value * value;
+            value--;
+            return -1f / 2f * (value * (value - 2f) - 1f);
         }
     }
 }
