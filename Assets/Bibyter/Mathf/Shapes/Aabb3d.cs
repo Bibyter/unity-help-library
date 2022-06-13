e62f8b9ce8d91941406ab7535ad385e42ebe62db
Bibyter <nikitatkarz@gmail.com>
2022-06-13 13:49:05 +0700

    update aabb

--- Assets/Bibyter/Mathf/Shapes/Aabb3d.cs
+++ Assets/Bibyter/Mathf/Shapes/Aabb3d.cs
@@ -21,5 +21,46 @@ namespace Bibyter.Mathematics
         {
             Gizmos.DrawWireCube(center, max - min);
         }
+
+        public bool Raycast(in Vector3 rayOrigin, in Vector3 rayDirection, out float distance)
+        {
+            return Raycast(rayOrigin, rayDirection, this, out distance);
+        }
+
+        public static bool Raycast(in Vector3 rayOrigin, in Vector3 rayDirection, in Aabb3d aabb, out float distance)
+        {
+            float t1 = (aabb.min.x - rayOrigin.x) / rayDirection.x;
+            float t2 = (aabb.max.x - rayOrigin.x) / rayDirection.x;
+            float t3 = (aabb.min.y - rayOrigin.y) / rayDirection.y;
+            float t4 = (aabb.max.y - rayOrigin.y) / rayDirection.y;
+            float t5 = (aabb.min.z - rayOrigin.z) / rayDirection.z;
+            float t6 = (aabb.max.z - rayOrigin.z) / rayDirection.z;
+
+            float tmin = Mathf.Max(Mathf.Max(Mathf.Min(t1, t2), Mathf.Min(t3, t4)), Mathf.Min(t5, t6));
+            float tmax = Mathf.Min(Mathf.Min(Mathf.Max(t1, t2), Mathf.Max(t3, t4)), Mathf.Max(t5, t6));
+
+            // if tmax < 0, ray (line) is intersecting AABB, but whole AABB is behing us
+            if (tmax < 0)
+            {
+                distance = -1f;
+                return false;
+            }
+
+            // if tmin > tmax, ray doesn't intersect AABB
+            if (tmin > tmax)
+            {
+                distance = -1;
+                return false;
+            }
+
+            if (tmin < 0f)
+            {
+                distance = tmax;
+                return true;
+            }
+
+            distance = tmin;
+            return true;
+        }
     }
 }
